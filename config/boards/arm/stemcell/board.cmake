778fcd1c1d7412c1891218420d9eb1c715e128ab
Mariappan Ramasamy <maari@basis-ai.com>
2022-02-07 00:09:35 +0800

    Update DMA config and board runners

--- config/boards/arm/stemcell/board.cmake
+++ config/boards/arm/stemcell/board.cmake
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 Mega Mind
 
+board_runner_args(pyocd "--target=stm32f401cc")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32F401CC" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
