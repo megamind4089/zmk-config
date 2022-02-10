19db96cb97c4ae954a2b817564b711eba655bd49
Mariappan Ramasamy <maari@basis-ai.com>
2022-02-10 09:09:06 +0800

    Working stemcell config

--- config/boards/arm/stemcell/board.cmake
+++ config/boards/arm/stemcell/board.cmake
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 Mega Mind
 
-board_runner_args(pyocd "--target=stm32f401cc")
+board_runner_args(pyocd "--target=stm32f411ce")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-board_runner_args(jlink "--device=STM32F401CC" "--speed=4000")
+board_runner_args(jlink "--device=STM32F411CE" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
